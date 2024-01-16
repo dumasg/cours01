8475d4839c5884f8e876593a7d28a85502946229
dumasg <geremydumas+github@gmail.com>
2024-01-16 10:41:49 +0100

    modification d'un des liens de la nav bar => fonctionnement ok

--- views/header.php
+++ views/header.php
@@ -23,7 +23,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../index.html">Accueil</a>
+            <a class="nav-link active" aria-current="page" href="../index.php">Accueil</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="../hobbie.php">Hobby</a>
