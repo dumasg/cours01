12ea0c60b3af3bc7c0bca01ad7cd2cc978f2212c
dumasg <geremydumas+github@gmail.com>
2024-01-16 10:49:03 +0100

    ajout d'un titre automatique

--- views/header.php
+++ views/header.php
@@ -1,3 +1,20 @@
+<?php
+
+    $pageCourant = $_SERVER['REQUEST_URI'];
+
+    $metaTitle = "Site CV";
+
+    echo($pageCourant);
+    if($pageCourant == "/" || $pageCourant == "/index.php"){
+        $metaTitle = "CV Gérémy";
+    }else if($pageCourant == "/hobbie.php"){
+        $metaTitle = "Mes hobbies";
+    }else if($pageCourant == "/contact.php"){
+        $metaTitle = "Contactez-moi !";
+    }
+
+?>
+
 <!doctype html>
 <html lang="fr">
 
@@ -5,7 +22,7 @@
   <meta charset="utf-8">
   <meta name="description" content="CV DevOps Pipeline déploiement continu Audit SEO Webapp">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>CODA - Gérémy</title>
+  <title><?= $metaTitle ?></title>
   <link rel="stylesheet" href="../assets/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
