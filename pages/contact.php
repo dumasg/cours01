b7ddc721bf9d863b7d50847666fb410cc091dce5
dumasg <geremydumas+github@gmail.com>
2024-01-17 10:09:06 +0100

    ajout de la vérification des champs remplit

--- pages/contact.php
+++ pages/contact.php
@@ -4,6 +4,8 @@
 
     $date = date("Y_m_d_H_i_s");
     $filename = "../contact/contact_".$date.".txt";
+    $formValad = true;
+
 
     
 
@@ -12,14 +14,28 @@
 <main class="mainFormulaire">
 
 <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){?>
-    <div>
-        <h2>Merci pour la soumission de votre formulaire !</h2>
-    </div>
+
     <?php 
-      $data = $_POST;    
-      $status = file_put_contents($filename, $data);
-      if($status){
-        ?> <p> Votre fichier est bien enregistré ! </p> <?php
+      $data = $_POST;
+      foreach ($data as $key => $value){
+          if(empty($value)){
+              $formValad = false;
+          }
+      }
+      if($formValad){
+          $status = file_put_contents($filename, $data);
+          if($status){
+            ?>
+              <div>
+                  <h2>Merci pour la soumission de votre formulaire !</h2>
+                  <p> Votre fichier est bien enregistré ! </p>
+              </div>
+              <?php
+          }else{
+              ?> <h3>Votre fichier n'a pas été enregistré !</h3> <?php
+          }
+      }else{
+          ?> <h2>Un des champs du formulaire a mal été rempli !</h2> <?php
       }
 
     ?>
